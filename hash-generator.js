55a51f47878d89cb621452f6e6558187766b8e5e
Zachary <zachary.zboncak@gmail.com>
2020-02-01 16:04:51 -0600

    refactored for JWTs

--- hash-generator.js
+++ hash-generator.js
@@ -2,9 +2,10 @@ const bcrypt = require('bcryptjs');
 
 let password = 'ping-password';
 
-let salt = '$2a$10$dXIUVa.20.ZzICuQjoaZo.';
-let hash = bcrypt.hashSync(password, salt);
+// let salt = bcrypt.genSaltSync(1);
+// let hash = bcrypt.hashSync(password, salt);
 
-console.log('salt', salt);
-console.log('hash', hash);
-console.log('match?', bcrypt.compareSync(password, hash));
+bcrypt.hash(password, 12, function(err, hash) {
+    console.log('hash', hash);
+    console.log('match?', bcrypt.compareSync(password, hash));
+});
